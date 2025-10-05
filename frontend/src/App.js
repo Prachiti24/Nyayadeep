c19cf5e5c7d893e90b288037cf60d87cdceb38fb
SreyaSudheeran <sreyasudheeran@gmail.com>
2025-10-05 22:20:19 +0530

    XP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./Pages/Login/LoginPage";
 import HomePage from "./Pages/Home";
-import ChatbotPage from "./Pages/Chatbot/ChatbotPage";
-import VerifyOtp from "./Pages/Login/VerifyOtp";
-import ChatbotPage from "./Pages/ChatbotPage";
-import VerifyOtp from "./Pages/VerifyOtp";
-import SpinWheelGame from "./Components/SpinWheelGame/SpinWheelGame";
 import ProfilePage from "./Pages/ProfilePage";
 import LandingPage from "./Pages/LandingPage";
+import ChatbotPage from "./Pages/Chatbot/ChatbotPage";
+import VerifyOtp from "./Pages/Login/VerifyOtp";
+import SpinWheelGame from "./Components/SpinWheelGame/SpinWheelGame";
 import PrivateRoute from "./Components/PrivateRoute";
 import CrosswordGame from "./Pages/Games/Crossword";
 import WordSearchGame from "./Pages/Games/WordSearch";
