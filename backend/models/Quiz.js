1e65a6c711a6225d54c43f45db5049266f881472
SreyaSudheeran <sreyasudheeran@gmail.com>
2025-10-30 20:30:52 +0530

    Dashboard connection

--- backend/models/Quiz.js
+++ backend/models/Quiz.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const QuizSchema = new Schema({
-    topics: [{ type: Schema.Types.ObjectId, ref: 'Topic' }],
-    number_of_questions: { type: Number, required: true },
-    total_xp: { type: Number, required: true }
+    part_id: { type : Number},
+    lesson_id: {type : Number}
+    //topics: [{ type: Schema.Types.ObjectId, ref: 'Topic' }],
+    // number_of_questions: { type: Number, required: true },
+    // total_xp: { type: Number, required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Quiz', QuizSchema);
