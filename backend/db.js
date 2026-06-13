ff551c649d868ed158ffe6c8fa977f99b47d3692
Prachiti24 <prachititelsinge@gmail.com>
2026-06-13 21:18:33 +0530

    Fix MongoDB Atlas connection

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 console.log(process.env.MONGODB_URI);
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+   // await mongoose.connect(process.env.MONGODB_URI);
+   await mongoose.connect(process.env.MONGO_URL);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err);
