4400080753c8360e2195c78fd001af8314f0017a
Prachiti24 <prachititelsinge@gmail.com>
2026-06-14 13:08:04 +0530

    fixed 0.0.0.0 in server.js

--- backend/server.js
+++ backend/server.js
@@ -129,4 +129,7 @@ app.post("/api/admin/send-fact-now", async (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
