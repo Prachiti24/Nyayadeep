b211253e0bb539e51608da0c1e056dd2ba31ada6
Prachiti24 <prachititelsinge@gmail.com>
2026-06-20 12:44:57 +0530

    fixed host name to relay

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -30,88 +30,42 @@ module.exports = sendEmail;*/
 
 const nodemailer = require("nodemailer");
 
-
-
 const sendEmail = async (options) => {
-
   try {
 
-
-
     const transporter = nodemailer.createTransport({
-
-      host: "smtp.brevo.com",   // ← CHANGE THIS
-
+      host: "smtp-relay.brevo.com",
       port: 587,
-
       secure: false,
 
-
-
       auth: {
-
         user: process.env.EMAIL_USERNAME,
-
         pass: process.env.EMAIL_PASSWORD,
-
       },
 
-
-
-      connectionTimeout: 10000,
-
-      greetingTimeout: 10000,
-
-      socketTimeout: 10000,
-
+      connectionTimeout: 30000,
+      greetingTimeout: 30000,
+      socketTimeout: 30000,
     });
-
-
 
     console.log("Connecting Brevo SMTP...");
 
-
-
-   // await transporter.verify();
-
-
-
-    console.log("SMTP Connected ✅");
-
-
-
     const info = await transporter.sendMail({
-
       from: `"Nyayadeep" <prachititelsinge@gmail.com>`,
-
       to: options.email,
-
       subject: options.subject,
-
       html: options.html,
-
     });
 
-
-
-    console.log("MAIL SENT:", info.messageId);
-
-
+    console.log("MAIL SENT ✅");
+    console.log(info.messageId);
 
     return true;
 
-
-
   } catch (error) {
-
     console.error("EMAIL ERROR:", error);
-
     return false;
-
   }
-
 };
-
-
 
 module.exports = sendEmail;
